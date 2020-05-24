e5f7ef4b41b2cb7676d739d7f68b402923555e1c
Calvert <wongcalvert@gmail.com>
2020-05-24 07:50:03 -0400

    Added explanation for staggered grid

--- recyclerview/src/main/java/com/calvert/recyclerview/staggeredrecyclerview/viewholder/StaggeredGridViewHolder.kt
+++ recyclerview/src/main/java/com/calvert/recyclerview/staggeredrecyclerview/viewholder/StaggeredGridViewHolder.kt
@@ -3,6 +3,7 @@ package com.calvert.recyclerview.staggeredrecyclerview.viewholder
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.calvert.mockdata.MockDataObject
 import com.calvert.recyclerview.databinding.ItemUserLayoutStaggeredGridBinding
 
@@ -12,7 +13,8 @@ class StaggeredGridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView
     fun bindView(mockDataObject: MockDataObject) {
         with(binding) {
             // loads random image
-            Glide.with(itemView.context).load("https://picsum.photos/${(200..500).random()}/${(300..500).random()}").into(ivStaggeredGrid)
+            Glide.with(itemView.context).load("https://picsum.photos/${(200..500).random()}/${(300..500).random()}")
+                .diskCacheStrategy(DiskCacheStrategy.ALL).into(ivStaggeredGrid)
             tvUserId.text = mockDataObject.id.toString()
             tvName.text = mockDataObject.name
             tvUsername.text = mockDataObject.username

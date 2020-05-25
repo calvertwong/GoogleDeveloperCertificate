61bcf8b2296d5d380302252a8a501dfaf41c489e
Calvert <wongcalvert@gmail.com>
2020-05-24 22:30:56 -0400

    Setup toolbar which enabled the back arrow functionality.

--- app/src/main/java/com/calvert/googledevelopercertificate/MainActivity.kt
+++ app/src/main/java/com/calvert/googledevelopercertificate/MainActivity.kt
@@ -4,28 +4,31 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.calvert.googledevelopercertificate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Keeps track of the current position of the graph and swaps destinations.
         // NavController needs to be told the path we want to go.
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        // BottomNavigationView is a common case to define multiple top-level destinations
+        // because the screens could be not related to each other.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_donate
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
     }
 }

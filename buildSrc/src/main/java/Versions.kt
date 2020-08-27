4d189f37de37c490739cf0e5e63d7484ecaad39f
Calvert <wongcalvert@gmail.com>
2020-08-26 23:35:50 -0500

    Added buildSrc module to take care of imported libraries

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,27 @@
+object Versions {
+    private const val kotlin_version = "1.3.72"
+    private const val nav_version = "2.3.0-beta01"
+
+    const val androidx_appcompat__appcompat: String = "1.2.0"
+    const val org_jetbrains_kotlin__kotlin_stdlib_jdk7: String = kotlin_version
+    const val androidx_core_core_ktx: String = "1.3.0"
+    const val com_google_android_material__material: String = "1.2.0"
+    const val androidx_constraintlayout_constraintlayout: String = "2.0.1"
+    const val androidx_vectordrawable__vectordrawable: String = "1.1.0"
+    const val androidx_navigation__navigation_fragment: String = nav_version
+    const val androidx_navigation__navigation_ui: String = nav_version
+    const val androidx_lifecycle__lifecycle_extensions: String =
+        "2.2.0"  // deprecated: https://developer.android.com/jetpack/androidx/releases/lifecycle
+    const val androidx_navigation__navigation_fragment_ktx: String = nav_version
+    const val androidx_navigation__navigation_ui_ktx: String = nav_version
+    const val junit__junit: String = "4.12"
+    const val androidx_test_ext__junit: String = "1.1.2"
+    const val androidx_test_espresso__espresso_core: String = "3.3.0"
+    const val androidx_multidex__multidex: String = "2.0.1"
+    const val androidx_legacy__legacy_support_v4: String = "1.0.0"
+    const val androidx_lifecycle__lifecycle_viewmodel_ktx: String = "2.2.0"
+    const val androidx_recyclerview__recyclerview: String = "1.1.0"
+    const val androidx_recyclerview__recyclerview_selection: String = "1.0.0"
+    const val com_github_bumptech_glide__glide: String = "4.11.0"
+    const val com_github_bumptech_glide__compiler: String = "4.11.0"
+}
